9561c853d273efe57f13eb6521a02eaeef54dd57
Helsi <helsi_@hotmail.com>
2025-03-27 09:06:54 -0400

    formatando api

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyATuE-rLN_PXvMvI3OGfcdB7lwN8xL1jmc",
-    authDomain: "whatsapclonee.firebaseapp.com",
-    projectId: "whatsapclonee",
-    storageBucket: "whatsapclonee.firebasestorage.app",
-    messagingSenderId: "219967339206",
-    appId: "1:219967339206:web:e8f2df2d440fd36629b5c7",
-    measurementId: "G-5BJVSTK20K"
+    apiKey: prosses.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: prosses.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: prosses.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: prosses.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: prosses.env.REACT_APP_FIREBASE_MESSEAGINGSENDERID,
+    appId: prosses.env.REACT_APP_FIREBASE_APPID,
+    measurementId: prosses.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
